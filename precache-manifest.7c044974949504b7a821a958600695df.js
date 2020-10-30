4ddab731d2f250c0a06c988f796be91502f6beab
PamelaBarboza <pamelarabarboza@gmail.com>
2020-10-29 22:42:07 -0300

    Updates

---
+++ precache-manifest.7c044974949504b7a821a958600695df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355d95aa018cfc3123b0af6f4705027b",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "8062ba4062b72582679b",
+    "url": "/portafolio/static/css/main.dea8d10e.chunk.css"
+  },
+  {
+    "revision": "0a3546072fc43e66a933",
+    "url": "/portafolio/static/js/2.e82853f8.chunk.js"
+  },
+  {
+    "revision": "378bd5ac050515df0b316e2fc55ae999",
+    "url": "/portafolio/static/js/2.e82853f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8062ba4062b72582679b",
+    "url": "/portafolio/static/js/main.2381240e.chunk.js"
+  },
+  {
+    "revision": "42584d6181d604f8c04c",
+    "url": "/portafolio/static/js/runtime-main.90e88b9f.js"
+  },
+  {
+    "revision": "01daa3b6f9f685ec286a0570a88f3067",
+    "url": "/portafolio/static/media/perfil.01daa3b6.jpg"
+  },
+  {
+    "revision": "63dca96df0900e40fa0f883ef66b2ba9",
+    "url": "/portafolio/static/media/proyecto_cinco.63dca96d.png"
+  },
+  {
+    "revision": "fd6623a72684c5f5a13862cd10200844",
+    "url": "/portafolio/static/media/proyecto_cuatro.fd6623a7.png"
+  },
+  {
+    "revision": "3732ed64878e5cfa1b8af7575bb463ec",
+    "url": "/portafolio/static/media/proyecto_dos.3732ed64.png"
+  },
+  {
+    "revision": "17b168f7a23e9906501df47f5d4d55d4",
+    "url": "/portafolio/static/media/proyecto_tres.17b168f7.png"
+  },
+  {
+    "revision": "9f445c9ceaeeb5c28ff8a3adc483db4b",
+    "url": "/portafolio/static/media/proyecto_uno.9f445c9c.png"
+  }
+]);
