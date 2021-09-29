1a3d0fb9b659f51d29d8833b9ab015f52bfb1ba5
PamelaBarboza <pamelarabarboza@gmail.com>
2021-09-29 13:26:31 -0300

    build

---
+++ my-app/build/precache-manifest.0c4dbf2952a9086d7ec8f20be450922f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2289d0222c18edd7cf6f94ccdff0abd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d556d3c18ad2c3dc6ea7",
+    "url": "/static/css/main.92c3066b.chunk.css"
+  },
+  {
+    "revision": "5f916722f0b4e29f00df",
+    "url": "/static/js/2.2232230f.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.2232230f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d556d3c18ad2c3dc6ea7",
+    "url": "/static/js/main.f4665efb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "8e428cc817d566a8c53cbbd2946d0bbd",
+    "url": "/static/media/PORTADA.8e428cc8.jpg"
+  },
+  {
+    "revision": "101cad587c134095808395a5f453c854",
+    "url": "/static/media/fonfon.101cad58.jpg"
+  },
+  {
+    "revision": "01daa3b6f9f685ec286a0570a88f3067",
+    "url": "/static/media/perfil.01daa3b6.jpg"
+  },
+  {
+    "revision": "63dca96df0900e40fa0f883ef66b2ba9",
+    "url": "/static/media/proyecto_cinco.63dca96d.png"
+  },
+  {
+    "revision": "fd6623a72684c5f5a13862cd10200844",
+    "url": "/static/media/proyecto_cuatro.fd6623a7.png"
+  },
+  {
+    "revision": "3732ed64878e5cfa1b8af7575bb463ec",
+    "url": "/static/media/proyecto_dos.3732ed64.png"
+  },
+  {
+    "revision": "17b168f7a23e9906501df47f5d4d55d4",
+    "url": "/static/media/proyecto_tres.17b168f7.png"
+  },
+  {
+    "revision": "9f445c9ceaeeb5c28ff8a3adc483db4b",
+    "url": "/static/media/proyecto_uno.9f445c9c.png"
+  },
+  {
+    "revision": "60392b9a892e2d1fefaf9e415d20293f",
+    "url": "/static/media/skill.60392b9a.png"
+  }
+]);
